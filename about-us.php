99f96723b06d8c54d4ed73bc3737f5ae58d80ac4
waleedamir571 <waleedamir063@gmail.com>
2022-12-16 01:50:24 +0500

    about page

---
+++ about-us.php
@@ -0,0 +1,59 @@
+<?php include 'header.php';?>
+
+<!-- end navbar -->
+<header class="page-header" data-background="images/slide01.jpg">
+	
+</header>
+
+
+<!-- end page-header -->
+<section class="content-section">
+  <div class="container">
+    <div class="row align-items-center">
+      <div class="col-lg-6">
+        <div class="section-title text-left">
+          <h2>WHY YOU CHOISE<br>
+            QUANCA ?</h2>
+          <p>Working out should be on your terms because life is busy enough and 
+            your time is precious. So we’ve <u>designed</u> a gym for people like you who 
+            want to get results without spending hours and hours at the gym. Also, 
+            to keep it simple and convenient to <strong>hit the gym</strong> you can use over 250 
+            gyms, 24/7 across the world. </p>
+        </div>
+        <!-- end section-title --> 
+      </div>
+      <!-- end col-6 -->
+      <div class="col-lg-5 offset-lg-1">
+      <img src="images/slide01.jpg" alt="">
+        <!-- end text-box -->
+       
+      </div>
+      <!-- end col-5 --> 
+    </div>
+    <!-- end row --> 
+  </div>
+  <!-- end container --> 
+</section>
+
+<section class="content-section" data-background="images/section-bg01.jpg" style="background: url(&quot;images/section-bg01.jpg&quot;);">
+  <div class="container">
+    <div class="row">
+      <div class="col-12 text-center">
+        <div class="cta-box wow fade animated" style="visibility: visible;">
+          <h2>GYM OWNER ?</h2>
+          <p>Sign up today and put your gym in front of 
+            millions of new customers.</p>
+          <a href="#" class="custom-button">BECOME PARTNER TODAY !</a> </div>
+        <!-- end cta-box --> 
+      </div>
+      <!-- end col-12 --> 
+    </div>
+    <!-- end row --> 
+  </div>
+  <!-- end container --> 
+</section>
+<br>
+<!-- end content-section -->
+
+<!-- end google-maps -->
+<?php include 'footer.php';?>
